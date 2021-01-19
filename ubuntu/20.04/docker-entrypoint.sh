953b5c315c113752b25c31db1915f4f4abc59e54
ikitten <ranql502@qq.com>
2021-01-19 19:41:25 +0800

    add hive 3.1.2

--- ubuntu/20.04/docker-entrypoint.sh
+++ ubuntu/20.04/docker-entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 /etc/init.d/ssh start
 
+HIVE_HOME=/usr/local/hive
 HADOOP_PATH=/usr/local/hadoop
 
 if [ "$1" = 'master' ]; then
@@ -31,6 +32,28 @@ if [ "$1" = 'master' ]; then
 
 	sleep 10
 	$HADOOP_PATH/bin/hadoop dfsadmin -safemode leave
+fi
+
+if [ "$1" = 'hive' ]; then
+	echo 'Running hive'
+	if [ ! -d "$HIVE_HOME/init" ]; then
+		touch $HIVE_HOME/init
+		echo 'Hive init process in progress...'
+		$HIVE_HOME/bin/schematool -dbType mysql -initSchema
+
+		echo
+		echo 'Hive init process done. Ready for start up.'
+		echo
+	fi
+
+	for f in /docker-entrypoint-initdb.d/*; do
+		case "$f" in
+			*.sh)  echo "$0: running $f"; . "$f" ;;
+			#*.sql) echo "$0: running $f"; "${mysql[@]}" < "$f" && echo ;;
+			*)     echo "$0: ignoring $f" ;;
+		esac
+		echo
+	done
 fi
 
 while : ; do sleep 1; done
